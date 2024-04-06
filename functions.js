2b692257b137ff1ffb6522a2755aee6d20c6ef2d
lililok <eshanu.sofia@yandex.ru>
2024-04-06 19:42:33 +0300

    calculator functions

---
+++ functions.js
@@ -0,0 +1,24 @@
+let firstNumberInput = prompt();
+let secondNumberInput = prompt();
+let operatorInput = prompt();
+
+
+function add(a, b) {
+    
+}
+
+function subtract(a, b) {
+    
+}
+
+function multiply(a, b) {
+    
+}
+
+function divide(a, b) {
+    
+}
+
+function operate(a, b, operator) {
+    
+}
