1cfa30c3ba30fe96c8d7a8c5df54c1acc63f89a6
lililok <eshanu.sofia@yandex.ru>
2024-04-11 16:45:59 +0300

    much cleaner need operator rework

--- functions.js
+++ functions.js
@@ -1,51 +1,73 @@
 const buttons = document.querySelectorAll("button");
 const display = document.querySelector('.display');
 let displayValue = '0';
-let operator = null;
-let firstValue = null;
-let secondValue = null;
-let secondValueStarted = false;
+let operator = '';
+let firstValue = '';
+let secondValue = '';
+let beginSecondValue = false;
 display.innerHTML = 0;
 
 function numberClick(number) {
-    
+    if (beginSecondValue) {
+        secondValue += number;
+        display.innerHTML = secondValue;
+    } else {
+        firstValue += number;
+        display.innerHTML = firstValue;
+    }
 }
 
-function operatorClick(operator) {
-    
+function operatorClick(op) {
+    operator = op;
+    beginSecondValue = true;
 }
 
-function clear() {
-    
+function clearing() {
+    firstValue = '';
+    secondValue = '';
+    operator = '';
+    beginSecondValue = false;
+    display.innerHTML = '0';
 }
 
-function add(a, b) {
-    return a+b;
+function add(firstValueParsed, secondValueParsed) {
+    return firstValueParsed+secondValueParsed;
 }
 
-function subtract(a, b) {
-    return a-b;
+function subtract(firstValueParsed, secondValueParsed) {
+    return firstValueParsed-secondValueParsed;
 }
 
-function multiply(a, b) {
-    return a*b;
+function multiply(firstValueParsed, secondValueParsed) {
+    return firstValueParsed*secondValueParsed;
 }
 
-function divide(a, b) {
-    return a/b;
+function divide(firstValueParsed, secondValueParsed) {
+    return firstValueParsed/secondValueParsed;
 }
 
 function operate(firstValue, secondValue, operator) {
     const firstValueParsed = parseInt(firstValue);
     const secondValueParsed = parseInt(secondValue);
+    let result;
     switch (operator) {
         case '+':
-            return add(firstValueParsed, secondValueParsed);
+            result = add(firstValueParsed, secondValueParsed);
+            break;
         case '-':
-            return subtract(firstValueParsed, secondValueParsed);
+            result = subtract(firstValueParsed, secondValueParsed);
+            break;
         case '*':
-            return multiply(firstValueParsed, secondValueParsed);
+            result = multiply(firstValueParsed, secondValueParsed);
+            break;
         case '/':
-            return divide(firstValueParsed, secondValueParsed);
+            result = divide(firstValueParsed, secondValueParsed);
+            break;
+        default:
+            return;
     }
+    display.innerHTML = result;
+    firstValue = result;
+    secondValue = '';
+    operator = '';
 }
